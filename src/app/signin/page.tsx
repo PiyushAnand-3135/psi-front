a397f95d28bb4824a70823551fc491dc79eb2613
Piyush Anand <149292519+PiyushAnand-3135@users.noreply.github.com>
2025-02-14 23:39:47 +0530

    fixed code

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 import React from "react";
 import Link from "next/link";
 import { signIn } from "next-auth/react";
@@ -9,7 +9,7 @@ const SigninPage = () => {
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
-            <div className="shadow-three mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]">
+            <div className="mx-auto max-w-[500px] rounded bg-white px-6 py-10 shadow-three dark:bg-dark sm:p-[60px]">
               <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
                 Sign in to your account
               </h3>
@@ -18,12 +18,12 @@ const SigninPage = () => {
               </p>
               <button
                 onClick={() => signIn("google")}
-                className="border-stroke dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary"
+                className="border-stroke mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:text-body-color-dark dark:shadow-two"
               >
                 Sign in with Google
               </button>
               <p className="text-center text-base font-medium text-body-color">
-                Don't have an account?{" "}
+                {"Don't have an account? "}
                 <Link href="/signup" className="text-primary hover:underline">
                   Sign up
                 </Link>
