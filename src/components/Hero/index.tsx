02ad538b9ffdc53e7eb288ffb0b6a93a3934990c
Piyush Anand <149292519+PiyushAnand-3135@users.noreply.github.com>
2025-02-15 23:15:26 +0530

    updated hero component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -11,16 +11,19 @@ const Hero = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
-                <h1 className="mb-5 text-9xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-7xl md:leading-tight">
-                Public Space AI
+                <h1 className="mb-5 text-5xl font-bold leading-tight text-black dark:text-white sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl">
+                  Public Space AI
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                Transform traditional surveillance into intelligent, real-time decision-making with PublicSpace AI. Enhance security, automate attendance, and optimize resources in schools, offices, malls, and more
+                  Transform traditional surveillance into intelligent, real-time
+                  decision-making with PublicSpace AI. Enhance security,
+                  automate attendance, and optimize resources in schools,
+                  offices, malls, and more
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
-                    className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80 hidden"
+                    className="hidden rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     🔥 Get Pro
                   </Link>
